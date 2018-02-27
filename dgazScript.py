f6813e6a7dddcc1b95a7ad8d3b3e3f27f2277505
Joshua Alan Kuhn <kuhnja@rose-hulman.edu>
2018-02-27 17:02:02 -0500

    set up hello world

--- dgazScript.py
+++ dgazScript.py
@@ -6,6 +6,11 @@ Created on Feb 26, 2018
 '''
 
 #This is going to make us a lot of money because we're hackers
+import gdax
+public_client = gdax.PublicClient()
 
 def main():
+    print("hello World!")
+
+if __name__ == "__main__":
     main()
